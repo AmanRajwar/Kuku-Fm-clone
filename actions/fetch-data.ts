7eb30fbf6476eed2ed130a64460ad71c185b0417
AmanRajwar <amanrajwar79@gmail.com>
2024-07-30 04:23:20 +0530

    done

--- actions/fetch-data.ts
+++ actions/fetch-data.ts
@@ -1,28 +1,34 @@
 'use server'
-let data1:any =  null;
-let data2:any = null;
+let data1: any = null;
+let data2: any = null;
 
 async function getData(index: number) {
-  const res = await fetch(`https://d31ntp24xvh0tq.cloudfront.net/api/v2.1/home/all/?preferred_langs=hindi&page=${index}&lang=english`)
-  return res.json()
+  try {
+    const res = await fetch(`https://d31ntp24xvh0tq.cloudfront.net/api/v2.1/home/all/?preferred_langs=hindi&page=${index}&lang=english`)
+    return res.json()
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+ 
 }
 
 
 export async function fetchShows(index: number) {
 
   try {
-    if (data1==null || data2==null) {
-       [data1, data2] = await Promise.all([getData(1), getData(2)])
+    if (data1 == null || data2 == null) {
+      [data1, data2] = await Promise.all([getData(1), getData(2)])
 
     }
-     if(index==1){
+    if (index == 1) {
       return data1;
-     }else{
+    } else {
       return data2;
-     }
-     
-    } catch (error) {
-  console.error("Error fetching data:", error);
-  return null;
-}
+    }
+
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
   }
+}
