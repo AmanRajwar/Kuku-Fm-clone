4e7df0c8e3c3ac7559e74762aa83ccae3cb2d323
AmanRajwar <amanrajwar79@gmail.com>
2024-07-30 06:23:52 +0530

    all done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div
-    className=' w-full max-sm:h-[200px] sm:h-[200px] h-[300px] flex-col z-50 flex items-center justify-center border-t border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8'
+    className=' mt-[40px] w-full max-sm:h-[200px] sm:h-[200px] h-[300px] flex-col z-50 flex items-center justify-center border-t border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8'
   >
     <h1 className='text-slate-300 font-extrabold'>Footer</h1>
     <h1 className='text-slate-300 font-extrabold'>- Time Limit Exceeded - </h1>
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -12,15 +12,15 @@ const MobileNav = () => {
     const pathname = usePathname();
 
     return (
-        <section className="lg:hidden w-full ml-6 max-w-[70px] sm:ml-0 sm:max-w-[180px]">
+        <section className="lg:hidden w-full ml-6 max-w-[70px] sm:ml-0 sm:max-w-[180px] ">
             <Sheet>
                 <SheetTrigger asChild>
-                  
+
                     <HamburgerSvg className="  max-sm:h-[50px] lg:hidden sm:ml-[120px] cursor-pointer " width={70} height={70} color="white" />
                 </SheetTrigger>
-                <SheetContent side="left" className="border-none bg-dark-1">
-                    <Link href="/" className="flex items-center gap-1">
-                        <Image className=" invert rounded-full"
+                <SheetContent side="left" className=" z-50 border-none  bg-dark-2 ">
+                    <Link href="/" className=" z-50 flex items-center gap-1">
+                        <Image className="  z-50 invert rounded-full"
                             src='/icons/kukufm.jpeg'
                             width={35}
                             height={35}
@@ -28,19 +28,19 @@ const MobileNav = () => {
                         />
 
                     </Link>
-                    <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+                    <div className=" z-50flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
                         <SheetClose asChild>
-                            <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                            <section className="z-50 flex h-full flex-col gap-6 pt-16 text-white">
                                 {navigation.map((item) => {
                                     const isActive = pathname === item.route;
-                                   {console.log(pathname,item.route)}
+                                    { console.log(pathname, item.route) }
                                     return (
                                         <SheetClose asChild key={item.route}>
                                             <Link
                                                 href={item.route}
                                                 key={item.id}
                                                 className={cn(
-                                                    'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
+                                                    ' z-50 flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
                                                     {
                                                         'bg-blue-1': isActive,
                                                     }
@@ -52,17 +52,23 @@ const MobileNav = () => {
                                                     width={20}
                                                     height={20}
                                                 /> */}
-                                                <p className="font-semibold">{item.title}</p>
+                                                <p className="font-semibold z-50">{item.title}</p>
                                             </Link>
+                                           
                                         </SheetClose>
+                                        
                                     );
                                 })}
                             </section>
+                            
+
                         </SheetClose>
-                    </div>
-                </SheetContent>
-            </Sheet>
-        </section>
+                    
+                </div>
+                <div className='absolute top-10 left-0 h-full w-full bg-carousel-gradient z-1 '></div>
+            </SheetContent>
+        </Sheet>
+        </section >
     );
 };
 
